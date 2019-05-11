4e5f945eb0599248cd84cb049f6bc4c20fc0f302
Shibin <shibinashraf36@gmail.com>
2019-05-11 13:08:38 +0530

    my first python programs

---
+++ ex1.py
@@ -0,0 +1,6 @@
+print("hello again")
+print("I Like typing this.")
+print("This is fun")
+#print("Yay! Printing")
+print("I'd much rather you 'not'.")
+print('I "Said" do not touch this.')
---
+++ ex2.py
@@ -0,0 +1,7 @@
+
+# a comment, this is so you can read your program later.
+# anything after the 
+# is ignored by python.
+
+print("i could have code like this.") # and the comment after is ignored
+print("this will run.")
